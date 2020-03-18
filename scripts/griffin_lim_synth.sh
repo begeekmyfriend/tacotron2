16799d1e08d35c8a8de1ab6eaff88cc27e0fc4bf
begeekmyfriend <begeekmyfriend@gmail.com>
2020-03-18 10:19:28 +0800

    Update
    Signed-off-by: begeekmyfriend <begeekmyfriend@gmail.com>

--- scripts/griffin_lim_synth.sh
+++ scripts/griffin_lim_synth.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python inference.py -o outputs --amp-run --speaker-num 4 --speaker-id 0 --log-file nvlog.json
+CUDA_VISIBLE_DEVICES=0 python inference.py -i text.txt -o outputs --amp-run --speaker-num 4 --speaker-id 0 --log-file nvlog.json
